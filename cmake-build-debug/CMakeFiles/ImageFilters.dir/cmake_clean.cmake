21fe0b81412326d04c655bb4d1fafdde452a0c38
Nada Mohammed <naddaa26@gmail.com>
2022-04-01 12:41:47 +0200

    \

---
+++ cmake-build-debug/CMakeFiles/ImageFilters.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ImageFilters.dir/main.cpp.obj"
+  "CMakeFiles/ImageFilters.dir/main.cpp.obj.d"
+  "ImageFilters.exe"
+  "ImageFilters.exe.manifest"
+  "ImageFilters.pdb"
+  "libImageFilters.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ImageFilters.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
